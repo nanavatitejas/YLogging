dc009e35f8c26d12f39c82e045d6313e1340f2a6
nanavatitejas <nanavatitejas@gmail.com>
2019-02-07 17:57:27 +0530

    Add files via upload

---
+++ File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  YLogging
+//
+//  Created by Tejash P on 07/02/19.
+//  Copyright © 2019 Tejash P. All rights reserved.
+//
+
+import Foundation
+
+public class File{
+    func add(num:Int, num1: Int)-> Int{
+        return num + num1
+    }
+}
---
+++ YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by Tejash P on 07/02/19.
+//  Copyright © 2019 Tejash P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
